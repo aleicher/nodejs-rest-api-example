b1568c35ee1837eed5ac6f9ed11a46734ff4648d
Andreas Leicher <andreas.leicher@mapcase.de>
2015-03-05 10:49:34 +0100

    add simple api

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
-var express = require('express');
-var app = express();
+var restify = require('restify');
+var citySave = require('save')('city');
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var server = restify.createServer({
+  name: 'my-api'
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+server.listen((process.env.PORT || 5000), function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
+
+server
+  .use(restify.fullResponse())
+  .use(restify.bodyParser());
+
+
+server.get('/cities', function (req, res, next) {
+  citySave.find({}, function (error, cities) {
+    res.send(cities)
+  })
+});
+
+
+server.post('/cities', function (req, res, next) {
+  if (req.params.name === undefined) {
+    return next(new restify.InvalidArgumentError('Name must be supplied'))
+  }
+ 
+  //citySave.create({ name: req.params.name }, function (error, city) {
+  citySave.create(req.params, function (error, city) {
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+ 
+    res.send(201, city)
+  })
+})
+
+
+server.get('/cities/:id', function (req, res, next) {
+  citySave.findOne({ _id: req.params.id }, function (error, city) {
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+ 
+    if (city) {
+      res.send(city)
+    } else {
+      res.send(404)
+    }
+  });
+});
+
+server.put('/cities/:id', function (req, res, next) {
+  if (req.params.name === undefined) {
+    return next(new restify.InvalidArgumentError('Name must be supplied'))
+  }
+ 
+  citySave.update({ _id: req.params.id, name: req.params.name }, function (error, city) {
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+    res.send()
+  });
+});
+
+server.del('/cities/:id', function (req, res, next) {
+  citySave.delete(req.params.id, function (error, city) {
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+ 
+    res.send()
+  });
 });
